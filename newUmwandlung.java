314a420bb13caded91ec809404a853169a36064a
leonsp02 <160969431+leonsp02@users.noreply.github.com>
2024-04-03 15:01:29 +0200

    Update newUmwandlung.java

--- newUmwandlung.java
+++ newUmwandlung.java
@@ -1560,6 +1560,7 @@ public class newUmwandlung {
     }
 
 
+    
     private static boolean laneExisting(String name, LaneSet laneSet) {
         for (Lane lane : laneSet.getLanes()) {
             if (lane.getName() != null && lane.getName().equals(name)) {
